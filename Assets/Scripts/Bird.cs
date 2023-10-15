92e73fd4af18827d2d5fb3d5dc6012363fb1bb5d
NKKM <135633452+MartynasOnTop@users.noreply.github.com>
2023-10-15 18:11:46 +0300

    GameEnd

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -10,12 +10,40 @@ public class Bird : MonoBehaviour
     public float rotatePower;
     public int score = 0;
     Rigidbody2D rb;
+    public GameObject endScreen;
+
+    public GameObject blueBird;
+    public GameObject redBird;
+    public GameObject yellowBird;
+
+    public GameObject day;
+    public GameObject night;
+
+    public float speed;
 
     public TMP_Text Points;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        Pipe.speed = speed;
+        endScreen.SetActive(false);
+
+        if (Random.Range(1, 3) == 1)
+        {
+            blueBird.SetActive(true);
+            day.SetActive(true);
+        }
+        else if (Random.Range(1, 3) == 2)
+        {
+            redBird.SetActive(true);
+            night.SetActive(true);
+        }
+        else
+        {
+            yellowBird.SetActive(true);
+            day.SetActive(true);
+        }
     }
 
     private void Update()
@@ -34,9 +62,22 @@ public class Bird : MonoBehaviour
     }
     void Die()
     {
-        var sceneName = SceneManager.GetActiveScene().name;
-        SceneManager.LoadScene(sceneName);
+        Pipe.speed = 0;
+        jumpSpeed = 0;
+        rb.velocity = Vector2.zero;
+        GetComponentInChildren<Animator>().enabled = false;
+
+        Invoke("ShowMenu", 1f);
+        //var sceneName = SceneManager.GetActiveScene().name;
+        //SceneManager.LoadScene(sceneName);
         score = 0;
+    }
+
+    void ShowMenu()
+    {
+        print("The End");
+        endScreen.SetActive(true);
+        Points.ClearMesh(true);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
